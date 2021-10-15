44fe91d7694e5b34cecfbf0c7d6acbdc2fc7bd31
Cine-Pixel <ikaumphriani@gmail.com>
2021-10-15 10:35:28 +0400

    quiz 1

--- heart.r
+++ heart.r
@@ -1,1 +1,10 @@
-df = read.csv("")
+df = read.csv("./datasets/Heart Attack Data Set.csv")
+dim(df)
+attributes(df)
+colnames(df)[1] = "age"
+
+df[1:5, c("age", "target")]
+head(df)
+df[1:5, ]
+dim(df[df$age > 50, ])[1]
+dim(df[df$age > 50 & df$sex==1, ])[1]

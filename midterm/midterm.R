69c7ef8947d0313dd9e30d0fe1359a6152ac623d
Cine-Pixel <ikaumphriani@gmail.com>
2021-12-17 10:42:11 +0400

    midterm1

--- midterm/midterm.R
+++ midterm/midterm.R
@@ -16,5 +16,5 @@ mean(iris[iris$Category == "A", ]$Petal.Width) - mean(iris[iris$Category == "B",
 #5
 br2 = c(1, 1.7, 5.5, 6)
 lab2 = c("A", "B", "C")
-iris$Category2 = cut(iris$Petal.Length, breaks = br2, labels = lab2, right = F)
+iris$Category2 = cut(iris$Petal.Length, breaks = br2, labels = lab2, right = F, include.lowest = T)
 max(iris[iris$Category2 == "B" & iris$Species == "setosa", ]$Sepal.Length)
